c20bf72aed665282a2e1700b9748a4d9606fc389
Maryna Serhiichuk <marina.serhiichuk@gmail.com>
2025-05-14 16:29:30 +0300

    contact-form-control: require fields, debounce

--- frontend/src/components/Entry/Entry.tsx
+++ frontend/src/components/Entry/Entry.tsx
@@ -1,7 +1,12 @@
 import { FieldAttributes } from "formik";
-import { FC } from "react";
-import { Field  } from 'formik';
+import { FC, Fragment } from "react";
+import { Field, ErrorMessage } from 'formik';
 
-export const Entry: FC<FieldAttributes<HTMLInputElement>> = (props) => {
-    return <Field {...props} className="focus:border-0 focus:shadow-none focus:outline-none focus:ring-0 w-full text-xl p-4 bg-stone-200" />
+export const Entry: FC<FieldAttributes<HTMLInputElement> & { errorAlert?: boolean }> = ({ errorAlert, ...props }) => {
+    return <Fragment>
+        <Field {...props} className="focus:border-0 focus:shadow-none focus:outline-none focus:ring-0 w-full text-xl p-4 bg-stone-200" />
+        {errorAlert &&
+            <ErrorMessage className="text-red-700" component="div" name={props.name} />
+        }
+    </Fragment>
 }
--- frontend/src/components/FormLabel/FormLabel.tsx
+++ frontend/src/components/FormLabel/FormLabel.tsx
@@ -1,7 +1,8 @@
+import classNames from "classnames"
 import { FC, PropsWithChildren } from "react"
 
-export const FormLabel: FC<PropsWithChildren<{ label?: string }>> = ({ children, label }) => {
-    return <div className="flex flex-col gap-2">
+export const FormLabel: FC<PropsWithChildren<{ label?: string, className?: string }>> = ({ children, label, className }) => {
+    return <div className={classNames("flex flex-col gap-2", className)}>
         <div className="text-xl font-semibold">
             {label}
         </div>
