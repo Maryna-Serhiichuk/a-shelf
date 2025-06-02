0724c0c4a9b7fdc20e11342aaece9e7ed5033b44
Maryna Serhiichuk <marina.serhiichuk@gmail.com>
2025-06-02 21:25:34 +0300

    workflow

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 BACKEND_DIR="./backend"
 HASHFILE="$BACKEND_DIR/.hash"
-
 NEW_HASH=$(cat $BACKEND_DIR/package.json $BACKEND_DIR/yarn.lock | md5sum | awk '{ print $1 }')
 
 if [ -f $HASHFILE ]; then
@@ -11,11 +10,11 @@ else
   OLD_HASH=""
 fi
 
-if [ "$NEW_HASH" != "$OLD_HASH" ]; then
+if [ "$DEPENDENCIES_CHANGED" = "true" ] || [ "$NEW_HASH" != "$OLD_HASH" ]; then
   echo "Changes to package.json or yarn.lock detected. Updating node_modules..."
   cd $BACKEND_DIR
   yarn install
   echo $NEW_HASH > $HASHFILE
 else
-  echo "There were no changes to package.json and yarn.lock. We are skipping node_modules updates."
+  echo "No changes to dependencies. Skipping yarn install."
 fi
