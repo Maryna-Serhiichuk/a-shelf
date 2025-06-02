ed3079a0e229363939e4eae4f4b52e0f9d747787
Maryna Serhiichuk <marina.serhiichuk@gmail.com>
2025-06-02 21:49:31 +0300

    workflow

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,14 @@ if [ -f $HASHFILE ]; then
 else
   OLD_HASH=""
 fi
-echo "1"
+
 if [ "$DEPENDENCIES_CHANGED" = "true" ] || [ "$NEW_HASH" != "$OLD_HASH" ]; then
   echo "Changes to package.json or yarn.lock detected. Updating node_modules..."
-  echo "2"
+  echo "Current directory is: $(pwd)"
+  echo "HERE................."
   cd $BACKEND_DIR
-  echo "3"
   yarn install
-  echo "4"
   cd -
-  echo "5"
   echo $NEW_HASH > $HASHFILE
 else
   echo "No changes to dependencies. Skipping yarn install."
