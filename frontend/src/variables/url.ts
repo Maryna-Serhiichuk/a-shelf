cc39e1f27fabead859261e20bc0787532fa17dd7
Maryna Serhiichuk <marina.serhiichuk@gmail.com>
2025-07-26 23:39:58 +0300

    fix hosts' names

--- frontend/src/variables/url.ts
+++ frontend/src/variables/url.ts
@@ -1,4 +1,4 @@
-const strapiUrl = process.env.NEXT_PUBLIC_HOST ?? 'http://127.0.0.1:1337'
+const strapiUrl = process.env.NEXT_SERVER_HOST ?? 'http://127.0.0.1:1337'
 
 const url = {
   strapi: strapiUrl
